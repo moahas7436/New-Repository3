a88102f16a178c70f25eb10ce8de9e6dde96dae2
moahas7436 <110007423+moahas7436@users.noreply.github.com>
2023-05-27 16:21:07 -0400

    Update helloworld.js

--- helloworld.js
+++ helloworld.js
@@ -7,8 +7,8 @@ http.createServer(function (request, response) {
    // Content Type: text/plain
    response.writeHead(200, {'Content-Type': 'text/plain'})
 
-   // Send the response body as "Hello World"
-   response.end('Hello Friends\n')
+   // Send the response body as "Welcome to my web application"
+   response.end('Welcome to my web application\n')
 }).listen(3333)
 
 // Console will print the message
